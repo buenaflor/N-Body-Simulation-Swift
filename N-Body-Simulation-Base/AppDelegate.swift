1465ab38da38803a00fe34e5209c1ec20a82f8fb
Giancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2020-06-28 02:19:31 +0200

    Initial Commit

---
+++ N-Body-Simulation-Base/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  N-Body-Simulation-Base
+//
+//  Created by Giancarlo Buenaflor on 28.06.20.
+//  Copyright © 2020 Giancarlo Buenaflor. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ N-Body-Simulation-Base/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  N-Body-Simulation-Base
+//
+//  Created by Giancarlo Buenaflor on 28.06.20.
+//  Copyright © 2020 Giancarlo Buenaflor. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
